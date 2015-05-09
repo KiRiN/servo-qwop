577188ce6d05feaaa9e3cca32c8914a631f5efb8
matsushima.hiroki <pi@raspberrypi.(none)>
2015-05-09 20:39:29 +0900

    update

--- qwop.js
+++ qwop.js
@@ -4,8 +4,13 @@ var app = require('http').createServer(handler)
 
 var config = JSON.parse(fs.readFileSync('./config.json', 'utf8'));
 
+// TODO config
+var thighsChannel = 1;
+var calvesChannel = 2;
+
 setDefaultRotation();
 app.listen(8080);
+console.log('Please access > http://hogehoge:8080');
 
 function handler (req, res) {
   fs.readFile(__dirname + '/index.html',
@@ -24,11 +29,24 @@ function setDefaultRotation() {
   // TODO
 };
 
+
+var devFile = '/dev/servoblaster';
+var writeStream = fs.createWriteStream(devFile);
+
 io.sockets.on('connection', function (socket) {
   console.log('connected');
   socket.emit('connected');
   socket.on('rotate', function(data) {
-    console.log(data);
+    var thighs = data.thighs;
+    var calves = data.calves;
+
+    var thighsMessage = thighsChannel + '=' + data.thighs + '%\n';
+    var calvelMessage = calvesChannel + '=' + data.calves + '%\n';
+    var message = thighsMessage + calvelMessage;
+
+    writeStream.write(message);
+    //console.log(message);
+
     socket.emit('response', data);
   });
 });
